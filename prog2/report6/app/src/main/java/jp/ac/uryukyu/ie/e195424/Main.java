1cac227f9fcad891c9b4cb1767414d75660d29bd
e195424 <73863165+e195424@users.noreply.github.com>
2021-01-07 00:34:56 +0900

    必要なクラスの追加

---
+++ prog2/report6/app/src/main/java/jp/ac/uryukyu/ie/e195424/Main.java
@@ -0,0 +1,17 @@
+package jp.ac.uryukyu.ie.e195424;
+
+class AttackPoke{
+    
+}
+class BlockPoke{
+ 
+    
+}
+class DamageCalc{
+
+}
+class Main {
+    public static void main(String[] srgs){
+        System.out.println();
+    }
+}
