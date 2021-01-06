907cadca6e4af3b2baf2b66393e6c2f110d61359
e195424 <73863165+e195424@users.noreply.github.com>
2021-01-07 00:39:36 +0900

    変数の定義
    プライベート化した変数をゲッターメソッドを利用して他クラスで扱えるようにした。

--- prog2/report6/app/src/main/java/jp/ac/uryukyu/ie/e195424/Main.java
+++ prog2/report6/app/src/main/java/jp/ac/uryukyu/ie/e195424/Main.java
@@ -1,10 +1,36 @@
 package jp.ac.uryukyu.ie.e195424;
 
 class AttackPoke{
-    
+    private int level;
+    private int power;
+    private String name;
+    AttackPoke(int l, int p, String n){
+        this.level=l;
+        this.power=p;
+        this.name=n;
+    }
+    void print(){
+        System.out.printf("レベル:%d\n", level);
+        System.out.printf("名前:%d\n", level);
+    }
+    public int getLevel(){
+        return level;
+    }
+    public int getPower(){
+        return power;
+    }
+    public String getName(){
+        return name;
+    }
 }
 class BlockPoke{
- 
+    private int defence;
+    BlockPoke(int b){
+        this.defence=b;
+    }
+    public int getDefence(){
+        return defence;
+    }
     
 }
 class DamageCalc{
